1f05792d5c8bae4dca27595adb7edfc5f359d8ea
kate20031 <83357098+kate20031@users.noreply.github.com>
2023-05-14 13:31:37 +0200

    Update game.py

--- backend/app/routers/game.py
+++ backend/app/routers/game.py
@@ -6,6 +6,20 @@ router = APIRouter(
     tags=["game"],
 )
 
+async def save_game_to_database(game_id, player_name, repo_name, repo_owner, start_time):
+    connection = await asyncpg.connect(
+        host="db",
+        user="gitguesser",
+        password="gitguesser",
+        database="gitguesser_db"
+    )
+    
+    sql = "INSERT INTO games (game_id, player_name, repo_name, repo_owner, start_time) VALUES ($1, $2, $3, $4, $5)"
+    values = (game_id, player_name, repo_name, repo_owner, start_time)
+
+    await connection.execute(sql, *values)
+
+    await connection.close()
 
 @router.post(
     "/",
@@ -13,7 +27,14 @@ router = APIRouter(
     description="Starts a new game.",
 )
 async def start_game(game_start_config: GameStartConfig):
-    pass
+    game_id = generate_game_id()
+    start_time = datetime.now()
+    
+    player_name = game_start_config.player_name
+    repo_name = game_start_config.repo_name
+    repo_owner = game_start_config.repo_owner
+    
+    save_game_to_database(game_id, player_name, repo_name, repo_owner, start_time)
 
 
 @router.get(
@@ -22,7 +43,31 @@ async def start_game(game_start_config: GameStartConfig):
     description="Retrieves information about a game with given id.",
 )
 async def get_game_info(id: int):
-    pass
+    connection = await asyncpg.connect(
+        host="db",
+        user="gitguesser",
+        password="gitguesser",
+        database="gitguesser_db"
+    )
+    
+    sql = "SELECT * FROM games WHERE id = $1"
+    values = (id,)
+    
+    result = await connection.fetchrow(sql, *values)
+    await connection.close()
+    
+    if result:
+        game_info = GameInfo(
+            game_id=result["id"],
+            repo_id=result["repository_id"],
+            player_name=result["player_name"],
+            repo_name=result["repo_name"],
+            repo_owner=result["repo_owner"],
+            start_time=result["start_time"]
+        )
+        return game_info
+    
+    raise HTTPException(status_code=404, detail="Game not found")
 
 
 @router.get(
@@ -31,9 +76,47 @@ async def get_game_info(id: int):
     description="Retrieves results of finished game with given id.",
 )
 async def get_game_results(id: int):
-    pass
+    connection = await asyncpg.connect(
+        host="db",
+        user="gitguesser",
+        password="gitguesser",
+        database="gitguesser_db"
+    )
+    
+    sql = "SELECT * FROM games WHERE id = $1"
+    values = (id,)
+    
+    result = await connection.fetchrow(sql, *values)
+    await connection.close()
+    
+    if result:
+        game_info = GameInfo(
+            end_time: datetime,
+            score: int,
+            player_answer: str,
+            correct_answer: str
+        )
+        return game_info
+    
+    raise HTTPException(status_code=404, detail="Game not found")
 
 
 @router.post("/{id}", description="Sends player answer for a game with given id.")
 async def send_answer(id: int, answer: PlayerAnswer):
-    pass
+    connection = await asyncpg.connect(
+        host="db",
+        user="gitguesser",
+        password="gitguesser",
+        database="gitguesser_db"
+    )
+    
+    sql = "UPDATE games SET player_answer = $1 WHERE id = $2"
+    values = (answer.answer, id)
+    
+    result = await connection.execute(sql, *values)
+    await connection.close()
+    
+    if result == "UPDATE 1":
+        return {"message": "Answer sent successfully"}
+    
+    raise HTTPException(status_code=404, detail="Game not found")
