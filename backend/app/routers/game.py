a29c9911460207d97cd96b4a63b8b79faf2559ac
kate20031 <83357098+kate20031@users.noreply.github.com>
2023-05-14 21:32:44 +0200

    Update game.py

--- backend/app/routers/game.py
+++ backend/app/routers/game.py
@@ -15,9 +15,8 @@ router = APIRouter(
     response_model=GameInfo,
     description="Starts a new game.",
 )
-async def start_game(game_start_config: GameStartConfig):
-    async with get_session() as session:
-        game_id = services.game_service.start_game(db = session, game_config = game_start_config)
+async def start_game(game_start_config: GameStartConfig, session: AsyncSession = Depends(get_session)):
+    game_id = services.game_service.start_game(db = session, game_config = game_start_config)
     return game_id
 
 
@@ -26,19 +25,18 @@ async def start_game(game_start_config: GameStartConfig):
     response_model=GameInfo,
     description="Retrieves information about a game with given id.",
 )
-async def get_game_info(id: int):
-    async with get_session() as session:
-        game = await services.game_service.get_game(db = session, game_id = id)
-        repo = await repository_service.get_repo(db = session, repo_id = game.repository_id)
+async def get_game_info(id: int, session: AsyncSession = Depends(get_session)):
+    game = await services.game_service.get_game(db = session, game_id = id)
+    repo = await repository_service.get_repo(db = session, repo_id = game.repository_id)
 
-        game_info = GameInfo(
-            game_id = id,
-            repo_id = repo.id,
-            player_name = game.player_name,
-            repo_name = repo.name,
-            repo_owner = repo.owner,
-            start_time = game.start_time,
-        )
+    game_info = GameInfo(
+        game_id = id,
+        repo_id = repo.id,
+        player_name = game.player_name,
+        repo_name = repo.name,
+        repo_owner = repo.owner,
+        start_time = game.start_time,
+    )
     return game_info
     
 
@@ -47,23 +45,21 @@ async def get_game_info(id: int):
     response_model=GameResults,
     description="Retrieves results of finished game with given id.",
 )
-async def get_game_results(id: int):
-    async with get_session() as session:
-        game_info = await get_game_info(id)
-        game = await game_service.get_game(db = session, game_id=id)
+async def get_game_results(id: int, session: AsyncSession = Depends(get_session)):
+    game_info = await get_game_info(id)
+    game = await game_service.get_game(db = session, game_id=id)
 
-        game_result = GameResults(
-            **dict(game_info), 
-            end_time = game.end_time, 
-            score = game.score, 
-            player_answer = game.player_answer,
-            correct_answer = game.correct_answer
-        ) 
+    game_result = GameResults(
+        **dict(game_info), 
+        end_time = game.end_time, 
+        score = game.score, 
+        player_answer = game.player_answer,
+        correct_answer = game.correct_answer
+    ) 
     return game_result
 
 
 @router.post("/{id}", description="Sends player answer for a game with given id.")
-async def send_answer(id: int, answer: PlayerAnswer):
-    async with get_session() as session:
-        game_service.give_answer(db=session, game_id=id, answer=answer)
+async def send_answer(id: int, answer: PlayerAnswer, session: AsyncSession = Depends(get_session)):
+    game_service.give_answer(db=session, game_id=id, answer=answer)
     return
