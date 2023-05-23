0c0696728a207487bd8528780004531d8bc5e507
kate20031 <83357098+kate20031@users.noreply.github.com>
2023-05-23 20:30:21 +0200

    Update repository.py

--- backend/app/schemas/repository.py
+++ backend/app/schemas/repository.py
@@ -8,3 +8,10 @@ class DirectoryInfo(BaseModel):
 
 class Directory(DirectoryInfo):
     subdirectories: list[DirectoryInfo]
+
+        
+class Repository(BaseModel):
+    repo_id: int
+    repo_name: str
+    repo_owner: str
+    repo_branch: str
