d94a31af86fdedd82e3c1f7bc85c89d956ddc896
kate20031 <83357098+kate20031@users.noreply.github.com>
2023-05-23 18:51:15 +0000

    Fixed style in routers/game.py

--- backend/app/routers/game.py
+++ backend/app/routers/game.py
@@ -1,12 +1,7 @@
 from app.dependencies import get_session
 from fastapi import APIRouter, Depends
-from schemas.game import (
-    GameInfo,
-    GameResults,
-    GameStartConfig,
-    GameWithId,
-    PlayerAnswer,
-)
+from schemas.game import (GameInfo, GameResults, GameStartConfig, GameWithId,
+                          PlayerAnswer)
 from sqlalchemy.ext.asyncio import AsyncSession
 
 router = APIRouter(
