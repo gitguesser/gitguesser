09780de285137410c8455eae892ab622f44f980a
kate20031 <83357098+kate20031@users.noreply.github.com>
2023-06-10 13:19:29 +0200

    fixed style

--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -1,4 +1,4 @@
-\import { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { BACKEND_URL } from "../config";
 
