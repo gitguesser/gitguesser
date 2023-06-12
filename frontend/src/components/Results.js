f738432477458cd3d6830043a9f98e3041128717
virtover <comodore9636@gmail.com>
2023-06-12 12:00:10 +0200

    requested changes

--- frontend/src/components/Results.js
+++ frontend/src/components/Results.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { BACKEND_URL } from "../config";
 
 function Results() {
@@ -8,46 +8,53 @@ function Results() {
   const [gameResults, setGameResults] = useState(null);
   const [repository, setRepository] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(false);
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchGameResults = async () => {
-      try {
-        const response = await fetch(
-          `${BACKEND_URL}/game/${state.gameId}/results/`
-        );
+    setLoading(true);
+    fetch(`${BACKEND_URL}/game/${state.gameId}/results`)
+      .then((response) => {
         if (response.ok) {
-          const data = await response.json();
-          setGameResults(data);
-        } else {
-          setError(true);
+          return response.json();
         }
-      } catch (error) {
-        setError(true);
-      }
-    };
-
-    const fetchRepository = async () => {
-      try {
-        const response = await fetch(
-          `${BACKEND_URL}/repository/${state.gameId}/`
-        );
-        if (response.ok) {
-          const data = await response.json();
-          setRepository(data);
-        } else {
-          setError(true);
-        }
-      } catch (error) {
-        setError(true);
-      } finally {
+        return response.json().then((e) => {
+          throw new Error(e.detail || "Unknown error");
+        });
+      })
+      .then((json) => {
+        setGameResults(json);
+        fetch(`${BACKEND_URL}/repository/${json.repository_id}`)
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            }
+            return response.json().then((e) => {
+              throw new Error(e.detail || "Unknown error");
+            });
+          })
+          .then((json) => {
+            console.log(json);
+            setRepository(json);
+            setLoading(false);
+          })
+          .catch((e) => {
+            console.log(e.message);
+            setError(e.message);
+            setLoading(false);
+          });
+      })
+      .catch((e) => {
+        console.log(e.message);
+        setError(e.message);
         setLoading(false);
-      }
-    };
-
-    fetchGameResults();
-    fetchRepository();
+      });
   }, [state.gameId]);
+
+  const returnHome = () => {
+    navigate("/");
+  };
 
   if (loading) {
     return <p>Loading...</p>;
@@ -73,13 +80,13 @@ function Results() {
       {gameResults && repository && (
         <div>
           <div>
-            repo: {repository.repo_owner}/{repository.repo_name}/
-            {repository.repo_branch}
+            repo: {repository.owner}/{repository.name}/{repository.branch}
           </div>
           <div>your answer: {gameResults.player_answer}</div>
           <div>correct answer: {gameResults.correct_answer}</div>
           <div>time: {formatDuration(duration)}</div>
           <div>score: {gameResults.score}</div>
+          <button onClick={returnHome}>OK</button>
         </div>
       )}
     </>
